e2a815a3a02912e1130212a5d0a9f2bff36b9931
pHequals7 <ce17b115@smail.iitm.ac.in>
2026-04-19 19:00:15 +0530

    Prepare release v0.6.0

--- scripts/build_native_app.sh
+++ scripts/build_native_app.sh
@@ -94,9 +94,9 @@ cat > "$STAGED_APP_DIR/Contents/Info.plist" <<PLIST
   <key>CFBundleIdentifier</key>
   <string>$BUNDLE_ID</string>
   <key>CFBundleVersion</key>
-  <string>0.5.6</string>
+  <string>0.6.0</string>
   <key>CFBundleShortVersionString</key>
-  <string>0.5.6</string>
+  <string>0.6.0</string>
   <key>CFBundleExecutable</key>
   <string>$APP_EXECUTABLE_NAME</string>
   <key>CFBundlePackageType</key>
